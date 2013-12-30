6d29ac3360b7e1a8848131ab8853e052248be46f
nekanat <nekanat.stock@gmail.com>
2013-12-30 18:11:47 +0900

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='SBIcomm', version='0.1.1',
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='SBIcomm', version='0.1.3',
       description='Python Interface for SBI Securities',
+      long_description=long_description,
       author='neka-nat',
       author_email='nekanat.stock@gmail.com',
       packages=find_packages(),
